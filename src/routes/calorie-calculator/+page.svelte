b2bded40aa7a439584a96be4c8049d85cf8f3fbe
Aliff Azmi <ku.aliff@comma.com.my>
2023-09-20 17:54:55 +0800

    code cleaning

--- src/routes/calorie-calculator/+page.svelte
+++ src/routes/calorie-calculator/+page.svelte
@@ -70,11 +70,6 @@ For example If you are sedentary, and your BMR equal 1745 so the total number of
           src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
           alt="Workflow"
         /> -->
-        <!-- <div class="scene inset-0 bg-inherit w-full h-80">
-          <Canvas>
-            <Scene />
-          </Canvas>
-        </div> -->
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">How much calories you need everyday?</h2>
         <p class="mt-2 text-center text-sm text-gray-600">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
